23ff894121aa9a1288c1a0f2d9d83aafed752270
nfdeveloper <nfdeveloper04@gmail.com>
2023-12-09 15:26:04 -0300

    Listando pessoas cadastradas

--- cousethymeleaf/src/main/java/br/com/nfdeveloper/cousethymeleaf/controller/PessoaController.java
+++ cousethymeleaf/src/main/java/br/com/nfdeveloper/cousethymeleaf/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.nfdeveloper.cousethymeleaf.model.Pessoa;
 import br.com.nfdeveloper.cousethymeleaf.repository.PessoaRepository;
@@ -23,5 +24,14 @@ public class PessoaController {
 	public String salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		return "cadastro/cadastropessoa"; 
+	}
+	
+	@GetMapping("/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		
+		andView.addObject("pessoas", pessoasIt);
+		return andView;
 	}
 }
