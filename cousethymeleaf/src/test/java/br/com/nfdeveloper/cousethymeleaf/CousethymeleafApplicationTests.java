52eedd313d61e010e8506823fcfed032fa0f671c
nfdeveloper <nfdeveloper04@gmail.com>
2023-12-09 11:28:02 -0300

    Primerio commit. Iniciando projeto.

---
+++ cousethymeleaf/src/test/java/br/com/nfdeveloper/cousethymeleaf/CousethymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nfdeveloper.cousethymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CousethymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
